596d19761d8049f4ccc0d7fbd13797b7d850918f
mark <mark.shoun@gmail.com>
2015-09-20 13:25:10 -0400

    Standardized naming convention for Ship status variables

--- Ship.py
+++ Ship.py
@@ -7,55 +7,55 @@ import pygame
 RADIANS_TO_DEGREES = 180 / math.pi
 
 class Ship:
-	def __init__(self, screen, position, angular_position):
+	def __init__(self, screen, position, position_angular):
 		self.screen = screen
-		self.x_center = 500
-		self.y_center = screen.get_rect().height / 2
+		self.center_x = 500
+		self.center_y = screen.get_rect().height / 2
 		self.image = pygame.image.load('images/ship.png').convert()
 		self.rect = self.image.get_rect()
 		
-		self.x_velocity, self.y_velocity = 0, 0
-		self.x_position, self.y_position = position
+		self.velocity_x, self.velocity_y = 0, 0
+		self.position_y, self.position_y = position
 		
-		self.angular_position = angular_position
-		self.angular_velocity = 0
+		self.velocity_angular = 0
+		self.position_angular = position_angular
 		self.finished = False
 	
 	def status(self):
-		speed = math.sqrt(self.x_velocity ** 2 + self.y_velocity ** 2)
+		speed = math.sqrt(self.velocity_x ** 2 + self.velocity_y ** 2)
 		return speed
 	
 	def camera_position(self):
-		x_absolute_offset = abs(self.x_velocity) ** .6 * 20
-		x_offset = math.copysign(x_absolute_offset, self.x_velocity)
+		absolute_offset_x = abs(self.velocity_x) ** .6 * 20
+		offset_x = math.copysign(absolute_offset_x, self.velocity_x)
 		
-		y_absolute_offset = abs(self.y_velocity) ** .6 * 20
-		y_offset = math.copysign(y_absolute_offset, self.y_velocity)
+		absolute_offset_y = abs(self.velocity_y) ** .6 * 20
+		offset_y = math.copysign(absolute_offset_y, self.velocity_y)
 		
-		return [x_offset - self.x_center + self.x_position,
-				y_offset - self.y_center + self.y_position]
+		return [offset_x - self.center_x + self.position_x,
+				offset_y - self.center_y + self.position_y]
 	
 	def position(self):
-		return (self.x_position, self.y_position)
+		return (self.position_x, self.position_y)
 	
 	def update(self):
 		self._handle_keyboard_input()
 
-		self.angular_position += self.angular_velocity
-		self.x_position += self.x_velocity
-		self.y_position += self.y_velocity
+		self.position_angular += self.velocity_angular
+		self.position_x += self.velocity_x
+		self.position_y += self.velocity_y
 		
 		return
 	
 	def _handle_keyboard_input(self):
 		pressed_keys = pygame.key.get_pressed()
 		if pressed_keys[pygame.K_LEFT]:
-			self.angular_velocity += .003
+			self.velocity_angular += .003
 		if pressed_keys[pygame.K_RIGHT]:
-			self.angular_velocity -= .003
+			self.velocity_angular -= .003
 		if pressed_keys[pygame.K_UP]:
-			self.y_velocity -= .1 * math.cos(self.angular_position)
-			self.x_velocity -= .1 * math.sin(self.angular_position)
+			self.velocity_y -= .1 * math.cos(self.position_angular)
+			self.velocity_x -= .1 * math.sin(self.position_angular)
 		if pressed_keys[pygame.K_ESCAPE]:
 			self.finished = True
 		
@@ -65,13 +65,13 @@ class Ship:
 		camera_x, camera_y = camera_position
 		
 		rotated_image = pygame.transform.rotozoom(self.image,
-												  self.angular_position * RADIANS_TO_DEGREES, 0.5)
+												  self.position_angular * RADIANS_TO_DEGREES, 0.5)
 		rotated_image.set_colorkey((0, 0, 0))
 		tmp_rect = rotated_image.get_rect()
 		rotated_rect = self.rect.inflate(tmp_rect.width - self.rect.width,
 										 tmp_rect.height - self.rect.height)
 		
-		rotated_rect.center = (self.x_position - camera_x, self.y_position - camera_y)
+		rotated_rect.center = (self.position_x - camera_x, self.position_y - camera_y)
 		
 		self.screen.blit(rotated_image, rotated_rect)
 		
