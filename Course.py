d6b9d3f658b0beed10e8b9dd374925c07e80da7b
mark <mark.shoun@gmail.com>
2015-09-21 22:35:59 -0400

    Refactored Gate and FinishBox objects into a unified Course object.

---
+++ Course.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import pygame
+
+import Gate
+import FinishBox
+
+class Course:
+    def __init__(self, panel):
+        self.gates = [Gate.Gate(panel, (0, 0), 90, Gate.STATUS_NEXT),
+                      Gate.Gate(panel, (50, 0), 90, Gate.STATUS_OTHER),
+                      Gate.Gate(panel, (60, 10), 0, Gate.STATUS_OTHER),
+                      Gate.Gate(panel, (60, 30), 0, Gate.STATUS_OTHER),
+                      Gate.Gate(panel, (50, 40), 90, Gate.STATUS_OTHER),
+                      Gate.Gate(panel, (0, 40), 90, Gate.STATUS_OTHER)]
+        self.finish_box = FinishBox.FinishBox(panel, (-8, 40))
+        
+        self.gate_sequence = [0, 1, 2, 3, 4, 5]
+        self.current_gate_index = 0
+        self.last_gate = 0
+        self.current_gate = self.gate_sequence[self.current_gate_index]
+    
+    def update(self, ship_position):
+        for gate in self.gates:
+            updated_status = gate.update(ship_position)
+            if updated_status:
+                self._update_gate_status()
+        
+        self.finish_box.update(ship_position)
+        return
+    
+    def _update_gate_status(self):
+        self._update_last_gate()
+        self._update_current_gate()
+        
+        self.current_gate_index += 1
+        self.last_gate = self.current_gate
+        
+        self._update_next_gate()
+        return
+    
+    def _update_last_gate(self):
+        if self.current_gate_index > 0:
+            self.gates[self.last_gate].status = Gate.STATUS_OTHER
+        return
+    
+    def _update_current_gate(self): 
+        self.gates[self.current_gate].status = Gate.STATUS_LAST
+        return
+    
+    def _update_next_gate(self):
+        if self.current_gate_index < len(self.gate_sequence):
+            self.current_gate = self.gate_sequence[self.current_gate_index]
+            self.gates[self.current_gate].status = Gate.STATUS_NEXT
+        return
+    
+    def draw(self, camera_position):
+        for gate in self.gates:
+            gate.draw(camera_position)
+        self.finish_box.draw(camera_position)
+        return
--- Level.py
+++ Level.py
@@ -4,8 +4,7 @@ import pygame
 
 import Ship
 import HUD
-import Gate
-import FinishBox
+import Course
 
 class Level:
     def __init__(self, screen):
@@ -17,49 +16,23 @@ class Level:
         
         self.ship = Ship.Ship(self.main_panel, (-0.1, 0), -90)
         self.hud = HUD.HUD(self.hud_panel)
-        self.gates = [Gate.Gate(self.main_panel, (0, 0), 90, Gate.STATUS_NEXT),
-                      Gate.Gate(self.main_panel, (50, 0), 90, Gate.STATUS_OTHER),
-                      Gate.Gate(self.main_panel, (60, 10), 0, Gate.STATUS_OTHER),
-                      Gate.Gate(self.main_panel, (60, 30), 0, Gate.STATUS_OTHER),
-                      Gate.Gate(self.main_panel, (50, 40), 90, Gate.STATUS_OTHER),
-                      Gate.Gate(self.main_panel, (0, 40), 90, Gate.STATUS_OTHER)]
-        self.finish_box = FinishBox.FinishBox(self.main_panel, (-8, 40))
-        
-        self.gate_sequence = [0, 1, 2, 3, 4, 5]
-        self.current_gate_index = 0
-        self.last_gate = 0
-        self.current_gate = self.gate_sequence[self.current_gate_index]
+        self.course = Course.Course(self.main_panel)
     
     def update(self):
         self.current_time = (pygame.time.get_ticks() - self.start_time) / 1000
         
         self.ship.update()
         self.camera_position = self.ship.camera_position()
-        for gate in self.gates:
-            updated_status = gate.update(self.ship.position())
-            if updated_status:
-                if self.current_gate_index > 0:
-                    self.gates[self.last_gate].status = Gate.STATUS_OTHER
-                    
-                self.current_gate_index += 1
-                self.gates[self.current_gate].status = Gate.STATUS_LAST
-                self.last_gate = self.current_gate
-                
-                if self.current_gate_index < len(self.gate_sequence):
-                    self.current_gate = self.gate_sequence[self.current_gate_index]
-                    self.gates[self.gate_sequence[self.current_gate]].status = Gate.STATUS_NEXT
-                    
-        self.hud.update(self.current_time - self.finish_box.timer, self.ship.status())
-        self.finish_box.update(self.ship.position())
+        
+        self.hud.update(self.current_time - self.course.finish_box.timer, self.ship.status())
+        self.course.update(self.ship.position())
         
         return
     
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.hud.draw()
-        for gate in self.gates:
-            gate.draw(self.camera_position)
-        self.finish_box.draw(self.camera_position)
+        self.course.draw(self.camera_position)
         self.ship.draw(self.camera_position)
         
         return
