db96d1fa5a95552cacbccc58dc92d151b570e76e
mark <mark.shoun@gmail.com>
2015-09-21 22:46:56 -0400

    Added LevelSplash class to handle fullscreen overlay messages

--- Level.py
+++ Level.py
@@ -5,6 +5,7 @@ import pygame
 import Ship
 import HUD
 import Course
+import LevelSplash
 
 class Level:
     def __init__(self, screen):
@@ -17,6 +18,7 @@ class Level:
         self.ship = Ship.Ship(self.main_panel, (-0.1, 0), -90)
         self.hud = HUD.HUD(self.hud_panel)
         self.course = Course.Course(self.main_panel)
+        self.level_splash = LevelSplash.LevelSplash(self.screen, "Go!", (255, 0, 255), 2.0)
     
     def update(self):
         self.current_time = (pygame.time.get_ticks() - self.start_time) / 1000
@@ -26,6 +28,7 @@ class Level:
         
         self.hud.update(self.current_time - self.course.finish_box.timer, self.ship.status())
         self.course.update(self.ship.position())
+        self.level_splash.update()
         
         return
     
@@ -34,5 +37,7 @@ class Level:
         self.hud.draw()
         self.course.draw(self.camera_position)
         self.ship.draw(self.camera_position)
+        if not self.level_splash.finished:
+            self.level_splash.draw()
         
         return
---
+++ LevelSplash.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pygame
+
+import config
+
+class LevelSplash:
+    def __init__(self, screen, text, color, time):
+        self.screen = screen
+        self.text = text
+        self.color = color
+        self.time = time
+        self.finished = False
+        
+        self.font = pygame.font.Font('fonts/Disco Nectar.ttf', 300)
+        self.text_surface = self.font.render(self.text, True, color)
+        self.rect = self.text_surface.get_rect()
+        self.rect.center = self.screen.get_rect().center
+    
+    def update(self):
+        self.time -= config.TICK_SIZE
+        if self.time < 0:
+            self.finished = True
+        return self.finished
+    
+    def draw(self):
+        self.screen.blit(self.text_surface, self.rect)
+        return
+    
