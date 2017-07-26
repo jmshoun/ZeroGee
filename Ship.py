af1c2a409a66ca3ff57e579a1d972d1c973113a4
Mark Shoun <mark.shoun@gmail.com>
2017-07-25 22:02:45 -0400

    Refactor ship engines into separate class
    Refactored the ship engines into their own classes. MainEngine and
    RotationEngine both inherit from Engine, and all Engines contain
    Flames. There are a few advantages to factoring engines out, but the
    chief difference from the player's perspective is that engine thrust
    and fuel consumption now ramps up and down in the same manner that the
    flame animations do. This makes the gameplay slightly more realistic,
    and better aligns the physics with the visuals.

--- Ship.py
+++ Ship.py
@@ -9,6 +9,9 @@ import config
 settings = config.DisplaySettings()
 RADIANS_TO_DEGREES = 180 / math.pi
 
+LEFT = 1
+RIGHT = -1
+
 
 class Ship(object):
     SCALE_FACTOR = 0.6
@@ -34,14 +37,11 @@ class Ship(object):
         self.rect = self.image.get_rect()
         
         self.fuel_mass = self.STARTING_FUEL_MASS
-        self.engine_on = False
-        self.left_thruster_on = False
-        self.right_thruster_on = False
-        self.main_flame = Flame(self.panel, 0, 35)
-        self.left_rear_flame = Flame(self.panel, -35, 15, math.pi / 2, 0.18)
-        self.right_rear_flame = Flame(self.panel, 35, 15, -math.pi / 2, 0.18)
-        self.left_front_flame = Flame(self.panel, -10, -15, math.pi / 2, 0.12)
-        self.right_front_flame = Flame(self.panel, 10, -15, -math.pi / 2, 0.12)
+        self.main_engine = MainEngine(self.panel, 4.0, 1040.0, 35, 0.4)
+        self.left_engine = RotationEngine(self.panel, 0.8, 1040.0, 2.0, (-35, 15), (10, -15),
+                                          0.12, LEFT)
+        self.right_engine = RotationEngine(self.panel, 0.8, 1040.0, 2.0, (35, 15), (-10, -15),
+                                           0.12, RIGHT)
 
         self.velocity_x, self.velocity_y = 0, 0
         self.position_x, self.position_y = position
@@ -66,39 +66,28 @@ class Ship(object):
         return self.position_x, self.position_y
     
     def update(self):
-        if self.fuel_mass > 0:
-            self._handle_keyboard_input()
+        self._handle_keyboard_input()
+
+        mass = self.DRY_MASS + self.fuel_mass
+        force = self.main_engine.get_force()
+        acceleration = force / mass / settings.meters_per_pixel * settings.tick_size
+        torque = self.left_engine.get_torque() - self.right_engine.get_torque()
+        acceleration_angular = torque / (mass * self.ROTATE_INERTIA_FACTOR) * settings.tick_size
+
+        self.velocity_angular += acceleration_angular
+        self.velocity_y -= acceleration * math.cos(self.position_angular)
+        self.velocity_x -= acceleration * math.sin(self.position_angular)
 
         self.position_angular += self.velocity_angular * settings.tick_size
         self.position_x += self.velocity_x * settings.tick_size
         self.position_y += self.velocity_y * settings.tick_size
-
-        self.main_flame.update(self.engine_on)
-        self.left_rear_flame.update(self.left_thruster_on)
-        self.right_front_flame.update(self.left_thruster_on)
-        self.right_rear_flame.update(self.right_thruster_on)
-        self.left_front_flame.update(self.right_thruster_on)
     
     def _handle_keyboard_input(self):
         pressed_keys = pygame.key.get_pressed()
-        mass = self.DRY_MASS + self.fuel_mass
-        acceleration = self.FORCE / mass / settings.meters_per_pixel * settings.tick_size
-        acceleration_angular = (self.TORQUE / (mass * self.ROTATE_INERTIA_FACTOR)
-                                * settings.tick_size)
-        
-        if pressed_keys[pygame.K_LEFT]:
-            self.velocity_angular += acceleration_angular
-            self.fuel_mass -= self.ROTATE_FUEL_RATE * settings.tick_size
-        if pressed_keys[pygame.K_RIGHT]:
-            self.velocity_angular -= acceleration_angular
-            self.fuel_mass -= self.ROTATE_FUEL_RATE * settings.tick_size
-        if pressed_keys[pygame.K_UP]:
-            self.velocity_y -= acceleration * math.cos(self.position_angular)
-            self.velocity_x -= acceleration * math.sin(self.position_angular)
-            self.fuel_mass -= self.THRUST_FUEL_RATE * settings.tick_size
-        self.engine_on = pressed_keys[pygame.K_UP] and self.fuel_mass > 0
-        self.left_thruster_on = pressed_keys[pygame.K_LEFT] and self.fuel_mass > 0
-        self.right_thruster_on = pressed_keys[pygame.K_RIGHT] and self.fuel_mass > 0
+        fuel_left = self.fuel_mass > 0
+        self.fuel_mass -= self.main_engine.update(fuel_left and pressed_keys[pygame.K_UP])
+        self.fuel_mass -= self.left_engine.update(fuel_left and pressed_keys[pygame.K_LEFT])
+        self.fuel_mass -= self.right_engine.update(fuel_left and pressed_keys[pygame.K_RIGHT])
     
     def draw(self, camera_position):
         camera_x, camera_y = camera_position
@@ -114,11 +103,79 @@ class Ship(object):
         rotated_rect.center = (self.position_x - camera_x, self.position_y - camera_y)
         
         self.panel.blit(rotated_image, rotated_rect)
-        self.main_flame.draw(rotated_rect.center, self.position_angular)
-        self.left_rear_flame.draw(rotated_rect.center, self.position_angular)
-        self.right_rear_flame.draw(rotated_rect.center, self.position_angular)
-        self.left_front_flame.draw(rotated_rect.center, self.position_angular)
-        self.right_front_flame.draw(rotated_rect.center, self.position_angular)
+        self.main_engine.draw(rotated_rect.center, self.position_angular)
+        self.left_engine.draw(rotated_rect.center, self.position_angular)
+        self.right_engine.draw(rotated_rect.center, self.position_angular)
+
+
+class Engine(object):
+    MAX_POWER = 10
+    NUM_STATES = 10
+
+    def __init__(self, panel, fuel_rate, exhaust_velocity):
+        self.panel = panel
+        self.fuel_rate = fuel_rate
+        self.exhaust_velocity = exhaust_velocity
+        self.engine_on = False
+        self.power = 0
+        self.state = -1
+
+    def update(self, key_on):
+        self.engine_on = key_on
+        if self.engine_on:
+            if self.power < self.MAX_POWER:
+                self.power += 1
+            if self.power == self.MAX_POWER:
+                self.state += 1
+                self.state %= self.NUM_STATES
+        else:
+            self.state = -1
+            if self.power > 0:
+                self.power -= 1
+        return self.power / self.MAX_POWER * self.fuel_rate * settings.tick_size
+
+    def _get_image_index(self):
+        if self.engine_on:
+            if self.power < self.MAX_POWER:
+                return self.power
+            else:
+                return self.MAX_POWER + self.state
+        elif self.power > 0:
+            return 2 * self.MAX_POWER + self.NUM_STATES - self.power
+        else:
+            return None
+
+
+class MainEngine(Engine):
+    def __init__(self, panel, fuel_rate, exhaust_velocity, offset_y, scale_factor):
+        super().__init__(panel, fuel_rate, exhaust_velocity)
+        self.full_force = self.fuel_rate * self.exhaust_velocity
+        self.flame = Flame(panel, 0, offset_y, scale_factor=scale_factor)
+
+    def get_force(self):
+        return self.full_force * self.power / self.MAX_POWER
+
+    def draw(self, center, angular_position):
+        index = self._get_image_index()
+        self.flame.draw(center, angular_position, index)
+
+
+class RotationEngine(Engine):
+    def __init__(self, panel, fuel_rate, exhaust_velocity, outboard_distance,
+                 fore_offset, aft_offset, scale_factor, direction):
+        super().__init__(panel, fuel_rate, exhaust_velocity)
+        self.outboard_distance = outboard_distance
+        self.full_torque = self.fuel_rate * self.exhaust_velocity * self.outboard_distance
+        self.fore_flame = Flame(panel, *fore_offset, direction * math.pi / 2, scale_factor)
+        self.aft_flame = Flame(panel, *aft_offset, -direction * math.pi / 2, scale_factor)
+
+    def get_torque(self):
+        return self.full_torque * self.power / self.MAX_POWER
+
+    def draw(self, center, angular_position):
+        index = self._get_image_index()
+        self.fore_flame.draw(center, angular_position, index)
+        self.aft_flame.draw(center, angular_position, index)
 
 
 class Flame(object):
@@ -129,29 +186,14 @@ class Flame(object):
         self.offset_angle = offset_angle
         self.scale_factor = scale_factor
         self.images = [self._load_image(i) for i in range(1, 31)]
-        self.current_index = None
-
-    def update(self, engine_on):
-        if self.current_index is None:
-            self.current_index = 1 if engine_on else None
-        elif self.current_index < 10:
-            self.current_index = self.current_index + 1 if engine_on else 29 - self.current_index
-        elif self.current_index < 20:
-            self.current_index = self.current_index + 1 if engine_on else 21
-        elif self.current_index == 20:
-            self.current_index = 10 if engine_on else 21
-        elif self.current_index < 30:
-            self.current_index = 31 - self.current_index if engine_on else self.current_index + 1
-        if self.current_index == 30:
-            self.current_index = None
 
     def _load_image(self, ndx):
-        filename = 'images/blue_flame/{0:04}.png'.format(ndx)
+        filename = "images/blue_flame/{0:04}.png".format(ndx)
         image = pygame.image.load(filename).convert()
         return pygame.transform.rotozoom(image, 0, self.scale_factor)
 
-    def draw(self, center, angular_position):
-        if not self.current_index:
+    def draw(self, center, angular_position, index):
+        if index is None:
             return
 
         center_x, center_y = center
@@ -160,8 +202,7 @@ class Flame(object):
                         center_y + self.offset_y * math.cos(angular_position)
                         - self.offset_x * math.sin(angular_position))
         angle = angular_position + self.offset_angle
-        rotated_image = pygame.transform.rotozoom(self.images[self.current_index],
-                                                  angle * RADIANS_TO_DEGREES,
+        rotated_image = pygame.transform.rotozoom(self.images[index], angle * RADIANS_TO_DEGREES,
                                                   settings.scale_factor)
         rotated_image.set_colorkey((0, 0, 0))
         rotated_rect = rotated_image.get_rect()
