0513da7dcf81c5191fd74f331bbd6c27ad959bc7
Mark Shoun <mark.shoun@gmail.com>
2017-08-01 20:59:55 -0400

    Create GateSet and ProxySet classes
    There was a lot of messy detail for the sets of gates and proxies
    within the Course class, so I refactored the class to contain separate
    GateSet and ProxySet classes that handle all of the details. Every
    Course will have both a GateSet and a ProxySet, regardless of whether
    it has any Gates (or Proxies). This avoids a lot of annoying
    conditionals within the Course class itself.

--- Course.py
+++ Course.py
@@ -11,28 +11,11 @@ class Course(object):
         with open(course_path) as course_file:
             course_dict = yaml.load(course_file.read())
 
-        if "gates" in course_dict.keys():
-            self.gates = [Gate.Gate.from_dict(panel, gate)
-                          for gate in course_dict["gates"]]
-            self.gates[0].status = Gate.Gate.STATUS_NEXT
-            self.gate_sequence = course_dict["gate_sequence"]
-        else:
-            self.gates = []
-            self.gate_sequence = []
-        if "proxies" in course_dict.keys():
-            self.proxies = [Gate.Proxy.from_dict(panel, proxy)
-                            for proxy in course_dict["proxies"]]
-        else:
-            self.proxies = []
+        gate_spec = course_dict.get("gates", []), course_dict.get("gate_sequence", [])
+        self.gate_set = GateSet(panel, *gate_spec)
+        proxy_spec = course_dict.get("proxies", [])
+        self.proxy_set = ProxySet(panel, proxy_spec)
         self.finish_box = FinishBox.FinishBox(panel, course_dict["finish_box"])
-
-        self.current_gate_index = 0
-        self.last_gate = 0
-        self.num_gates = len(self.gate_sequence)
-        self.num_proxies = len(self.proxies)
-        self.proxies_left = self.num_proxies
-        if self.num_gates:
-            self.current_gate = self.gate_sequence[self.current_gate_index]
 
     @property
     def bounding_box(self):
@@ -41,55 +24,99 @@ class Course(object):
 
     def _extremum(self, func, member):
         extreme = getattr(self.finish_box.position, member)
-        if len(self.gates):
+        if self.gates:
             extreme_gate = func([getattr(gate.position, member) for gate in self.gates])
             extreme = func(extreme, extreme_gate)
-        if len(self.proxies):
+        if self.proxies:
             extreme_proxy = func([getattr(proxy.position, member) for proxy in self.proxies])
             extreme = func(extreme, extreme_proxy)
         return extreme
     
     def update(self, ship_position):
-        for gate in self.gates:
-            updated_status = gate.update(ship_position)
-            if updated_status:
-                self._update_gate_status()
-        for proxy in self.proxies:
-            if proxy.update(ship_position):
-                self.proxies_left -= 1
+        self.gate_set.update(ship_position)
+        self.proxy_set.update(ship_position)
 
-        if self.current_gate_index == len(self.gates) and self.proxies_left == 0:
+        if self.gate_set.is_complete and self.proxy_set.is_complete:
             self.finish_box.locked = False
         self.finish_box.update(ship_position)
 
     @property
     def status(self):
-        return self.current_gate_index, self.num_gates
+        return self.gate_set.current_gate_index, self.gate_set.num_gates
     
-    def _update_gate_status(self):
-        self._update_last_gate()
-        self._update_current_gate()
-        
-        self.current_gate_index += 1
-        self.last_gate = self.current_gate
-        
-        self._update_next_gate()
-    
+    def draw(self, camera_position):
+        self.gate_set.draw(camera_position)
+        self.proxy_set.draw(camera_position)
+        self.finish_box.draw(camera_position)
+
+    @property
+    def gates(self):
+        return self.gate_set.gates
+
+    @property
+    def proxies(self):
+        return self.proxy_set.proxies
+
+
+class ProxySet(object):
+    def __init__(self, panel, proxies):
+        self.proxies = [Gate.Proxy.from_dict(panel, proxy) for proxy in proxies]
+        self.num_proxies = len(self.proxies)
+        self.proxies_completed = 0
+
+    def update(self, ship_position):
+        for proxy in self.proxies:
+            if proxy.update(ship_position):
+                self.proxies_completed += 1
+
+    def draw(self, camera_position):
+        for proxy in self.proxies:
+            proxy.draw(camera_position)
+
+    @property
+    def is_complete(self):
+        return self.proxies_completed == self.num_proxies
+
+
+class GateSet(object):
+    def __init__(self, panel, gates, gate_sequence):
+        self.gates = [Gate.Gate.from_dict(panel, gate) for gate in gates]
+        self.gate_sequence = gate_sequence
+        self.num_gates = len(self.gate_sequence)
+        self.current_gate_index = 0
+        if self.gates:
+            self.current_gate = self.gate_sequence[self.current_gate_index]
+            self.last_gate = self.current_gate
+            self.gates[self.current_gate].status = Gate.Gate.STATUS_NEXT
+
+    def update(self, ship_position):
+        if not self.gates:
+            return
+        updated_status = self.gates[self.current_gate].update(ship_position)
+        if updated_status:
+            self._update_last_gate()
+            self._update_current_gate()
+            self._update_next_gate()
+
     def _update_last_gate(self):
         if self.current_gate_index > 0:
             self.gates[self.last_gate].status = Gate.Gate.STATUS_OTHER
-    
-    def _update_current_gate(self): 
-        self.gates[self.current_gate].status = Gate.Gate.STATUS_LAST
-    
-    def _update_next_gate(self):
-        if self.current_gate_index < len(self.gate_sequence):
+        self.last_gate = self.current_gate
+        self.gates[self.last_gate].status = Gate.Gate.STATUS_LAST
+
+    def _update_current_gate(self):
+        self.current_gate_index += 1
+        if self.current_gate_index < self.num_gates:
             self.current_gate = self.gate_sequence[self.current_gate_index]
+
+    def _update_next_gate(self):
+        if not self.is_complete:
             self.gates[self.current_gate].status = Gate.Gate.STATUS_NEXT
-    
+
     def draw(self, camera_position):
         for gate in self.gates:
             gate.draw(camera_position)
-        for proxy in self.proxies:
-            proxy.draw(camera_position)
-        self.finish_box.draw(camera_position)
+
+    @property
+    def is_complete(self):
+        return self.current_gate_index == len(self.gate_sequence)
