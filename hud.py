dd6ddbc552cae725a9114345f9e37918e9c7f8d3
Mark Shoun <mark.shoun@gmail.com>
2017-08-09 23:47:39 -0400

    Overhaul HUD design
    Overhauled the design of the game HUD to fill out the available space
    more, make it easier to add additional fields, and improve readability.
    
    Some fields on the HUD (notably Split and Proxies) are dummies that
    have not been implemented yet; others (like Fuel) still have some major
    overhauling left, and the whole HUD still has myriad tweaks of sizes,
    positions, and colors ahead, but this new layout is much closer to the
    final version.

--- hud.py
+++ hud.py
@@ -5,6 +5,11 @@ import text
 
 
 class HUD(object):
+    TIME_VERTICAL_OFFSET = 0.0
+    WAYPOINT_VERTICAL_OFFSET = 0.27
+    SPEED_VERTICAL_OFFSET = 0.6
+    FUEL_VERTICAL_OFFSET = 0.8
+
     def __init__(self, panel):
         self.time = 0
         self.gate_number = 0
@@ -12,7 +17,7 @@ class HUD(object):
         self.speed = 0
         self.panel = panel
         self.fuel_mass = None
-        self.primary_font = pygame.freetype.Font('fonts/digital-7 (mono italic).ttf')
+        self.primary_font = pygame.freetype.Font('fonts/AlphaSmart3000.ttf')
     
     def update(self, time, course_status, ship_status):
         self.time = time
@@ -21,20 +26,74 @@ class HUD(object):
         self.fuel_mass = max(self.fuel_mass, 0)
     
     def draw(self):
-        gate_text = "{0:02d} / {1:02d}".format(self.gate_number, self.total_gates)
-        time_text = "{0:>05.2f}".format(self.time)
+        self._draw_waypoint()
+        self._draw_time()
+        self._draw_speed()
+        self._draw_fuel()
+
+    def _draw_waypoint(self):
+        waypoint_text = "{0:02d} / {1:02d}".format(self.gate_number, self.total_gates)
+        gate_text = "{0:02d}/{1:02d}".format(self.gate_number, self.total_gates)
+        proxy_text = "--/--"
+
+        text.render_text(self.panel, self.primary_font, "Waypoints:",
+                         (0.50, self.WAYPOINT_VERTICAL_OFFSET + .04),
+                         foreground=text.BLUE, size=1.5, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, waypoint_text,
+                         (0.50, self.WAYPOINT_VERTICAL_OFFSET + 0.14),
+                         size=2.5, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, "Gates:",
+                         (0.25, self.WAYPOINT_VERTICAL_OFFSET + 0.22),
+                         foreground=text.BLUE, size=0.8, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, "Proxies:",
+                         (0.75, self.WAYPOINT_VERTICAL_OFFSET + 0.22),
+                         foreground=text.BLUE, size=0.8, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, gate_text,
+                         (0.25, self.WAYPOINT_VERTICAL_OFFSET + 0.28),
+                         size=1.2, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, proxy_text,
+                         (0.75, self.WAYPOINT_VERTICAL_OFFSET + 0.28),
+                         size=1.2, justify=text.CENTER)
+
+    def _draw_speed(self):
         speed_text = "{0:>05.2f}".format(self.speed)
+
+        text.render_text(self.panel, self.primary_font, "Velocity:",
+                         (0.50, self.SPEED_VERTICAL_OFFSET + 0.03),
+                         foreground=text.BLUE, size=1.2, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, speed_text,
+                         (0.40, self.SPEED_VERTICAL_OFFSET + 0.16), size=2.4, justify=text.BOTTOM)
+        text.render_text(self.panel, self.primary_font, "m/s",
+                         (0.75, self.SPEED_VERTICAL_OFFSET + 0.16), size=1.5, justify=text.BOTTOM,
+                         foreground=text.BLUE)
+
+    def _draw_fuel(self):
         fuel_text = "{0:>05.1f}".format(self.fuel_mass)
 
-        text.render_text(self.panel, self.primary_font, "GATE NUMBER:", (0.05, 0.05),
-                         foreground=text.BLUE, size=1.2)
-        text.render_text(self.panel, self.primary_font, gate_text, (0.05, 0.12), size=2)
-        text.render_text(self.panel, self.primary_font, "TIME:", (0.05, 0.25),
-                         foreground=text.BLUE, size=1.2)
-        text.render_text(self.panel, self.primary_font, time_text, (0.05, 0.32), size=2)
-        text.render_text(self.panel, self.primary_font, "SPEED:", (0.05, 0.45),
-                         foreground=text.BLUE, size=1.2)
-        text.render_text(self.panel, self.primary_font, speed_text, (0.05, 0.52), size=2)
-        text.render_text(self.panel, self.primary_font, "FUEL LEFT:", (0.05, 0.65),
-                         foreground=text.BLUE, size=1.2)
-        text.render_text(self.panel, self.primary_font, fuel_text, (0.05, 0.72), size=2)
+        text.render_text(self.panel, self.primary_font, "Fuel Left:",
+                         (0.50, self.FUEL_VERTICAL_OFFSET + 0.03),
+                         foreground=text.BLUE, size=1.2, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, fuel_text,
+                         (0.40, self.FUEL_VERTICAL_OFFSET + 0.16), size=2.4, justify=text.BOTTOM)
+        text.render_text(self.panel, self.primary_font, "kg",
+                         (0.72, self.FUEL_VERTICAL_OFFSET + 0.16), size=1.5, justify=text.BOTTOM,
+                         foreground=text.BLUE)
+
+    def _draw_time(self):
+        time_text = self.format_time(self.time)
+
+        text.render_text(self.panel, self.primary_font, time_text,
+                         (0.50, self.TIME_VERTICAL_OFFSET + .06),  size=3.0, justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, "Split:",
+                         (0.45, self.TIME_VERTICAL_OFFSET + 0.17), foreground=text.BLUE, size=1.0,
+                         justify=text.CENTER)
+        text.render_text(self.panel, self.primary_font, "00:00.00",
+                         (0.95, self.TIME_VERTICAL_OFFSET + 0.17), size=1.0, justify=text.RIGHT)
+        text.render_text(self.panel, self.primary_font, "+00:00.00",
+                         (0.95, self.TIME_VERTICAL_OFFSET + 0.23), size=1.0, justify=text.RIGHT)
+
+    @staticmethod
+    def format_time(time):
+        time_minutes = int(time // 60)
+        time_seconds = time % 60
+        return "{0:02d}:{1:>05.2f}".format(time_minutes, time_seconds)
--- text.py
+++ text.py
@@ -1,3 +1,5 @@
+import pygame
+
 import config
 
 settings = config.DisplaySettings()
@@ -10,6 +12,8 @@ reference_font_size = FONT_SIZE_LOOKUP[settings.screen_resolution]
 
 TOP_LEFT = (0, 0)
 CENTER = (0.5, 0.5)
+BOTTOM = (0.5, 1.0)
+RIGHT = (1.0, 0.5)
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -23,12 +27,12 @@ def render_text(surface, font, text, location, foreground=WHITE, background=BLAC
     location_px = (location[0] * surface_size[0],
                    location[1] * surface_size[1])
 
-    if justify == TOP_LEFT:
-        font.render_to(surface, location_px, text, fgcolor=foreground, bgcolor=background,
-                       size=size_px)
-    else:
-        text_surface = font.render(text, foreground, background, size=size_px)
-        text_size = text_surface.get_size()
-        location_offset = (text_size[0] * justify[0], text_size[1] * justify[1])
-        location_px = (location_px[0] - location_offset[0], location_px[1] - location_offset[1])
-        surface.blit(text_surface, location_px)
+    pad_width = font.get_rect("/", size=size_px).width
+    raw_surface, raw_rect = font.render("/" + text + "/", foreground, background, size=size_px)
+    text_surface = raw_surface.subsurface(pygame.Rect(pad_width, 0,
+                                                      raw_rect.width - 2 * pad_width,
+                                                      raw_rect.height))
+    text_rect = text_surface.get_rect()
+    location_offset = (text_rect.width * justify[0], text_rect.height * justify[1])
+    location_px = (location_px[0] - location_offset[0], location_px[1] - location_offset[1])
+    surface.blit(text_surface, location_px)
