a72b82471052583947b99e940305d85e253fbb56
Mark Shoun <mark.shoun@gmail.com>
2017-08-15 16:15:38 -0400

    Save all runs on each course
    Modified the Profile class to save all runs on each course, regardless
    of whether or not they are bests. This will be handy later on for deep
    analyses on optimum configuration setups.

--- ZeroGee.py
+++ ZeroGee.py
@@ -24,8 +24,8 @@ with open(course_name + ".yaml") as course_file:
     course_dict = yaml.load(course_file.read())
 with open(sys.argv[2]) as ship_file:
     ship_dict = yaml.load(ship_file.read())
-best_split_dict = player_profile.course_bests.get(course_name)
-level = level.Level(screen, course_dict, ship_dict, best_split_dict)
+best_splits = player_profile.fastest_run_splits(course_dict)
+level = level.Level(screen, course_dict, ship_dict, best_splits)
 
 
 def check_for_termination():
@@ -41,8 +41,5 @@ while not check_for_termination():
     pygame.display.flip()
     clock.tick(1 / settings.tick_size)
 
-last_split_dict = level.active_splits.as_dict()
-if not best_split_dict or last_split_dict["final_time"] < best_split_dict["final_time"]:
-    player_profile.course_bests[course_name] = last_split_dict
+player_profile.add_record(course_dict, ship_dict, level.active_splits)
 player_profile.save()
-
--- profile.py
+++ profile.py
@@ -1,14 +1,20 @@
 import pickle
+import json
+import hashlib
+
+import waypoint
 
 
 class Profile(object):
-    def __init__(self, name, course_bests=None):
+    def __init__(self, name, run_records=None):
         self.name = name
-        self.course_bests = course_bests if course_bests else {}
+        self.run_records = run_records if run_records else {}
 
     @classmethod
     def from_dict(cls, dict_):
-        return cls(dict_["name"], dict_["course_bests"])
+        run_records = {k: [RunRecord.from_dict(e) for e in v]
+                       for k, v in dict_["run_records"].items()}
+        return cls(dict_["name"], run_records)
 
     @classmethod
     def load(cls, name):
@@ -21,12 +27,51 @@ class Profile(object):
 
     def as_dict(self):
         return {"name": self.name,
-                "course_bests": self.course_bests}
+                "run_records": {k: [e.as_dict() for e in v]
+                                for k, v in self.run_records.items()}}
 
     def save(self):
         with open(self.filename_from_name(self.name), "wb") as file:
             pickle.dump(self.as_dict(), file)
 
+    def add_record(self, course_dict, ship_dict, splits):
+        course_hash = self.dict_hash(course_dict)
+        if course_hash not in self.run_records:
+            self.run_records[course_hash] = []
+        self.run_records[course_hash] += [RunRecord(ship_dict, splits)]
+
+    def fastest_run_splits(self, course_dict):
+        course_hash = self.dict_hash(course_dict)
+        if course_hash not in self.run_records.keys():
+            return None
+        sorted_records = sorted(self.run_records[course_hash], key=lambda record: record.run_time)
+        return sorted_records[0].splits
+
     @staticmethod
     def filename_from_name(name):
         return "profiles/{}.p".format(name)
+
+    @staticmethod
+    def dict_hash(dict_):
+        dict_string = json.dumps(dict_, sort_keys=True).encode("utf-8")
+        hash_obj = hashlib.sha256(dict_string)
+        return hash_obj.digest()
+
+
+class RunRecord(object):
+    def __init__(self, ship_dict, splits):
+        self.ship_dict = ship_dict
+        self.splits = splits
+
+    @classmethod
+    def from_dict(cls, dict_):
+        splits = waypoint.Splits.from_dict(dict_["splits"])
+        return cls(dict_["ship_dict"], splits)
+
+    def as_dict(self):
+        return {"ship_dict": self.ship_dict,
+                "splits": self.splits.as_dict()}
+
+    @property
+    def run_time(self):
+        return self.splits.final_time
